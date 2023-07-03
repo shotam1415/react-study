ec96c78c5a6ace0493dd3a4ed4ad8157b9698063
shotam1415 <shotam1415@gmail.com>
2023-07-03 18:55:34 +0900

    パラメータに応じて出力されるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
 
-
 const changeQueryParam = (e:React.ChangeEvent<HTMLInputElement>)=>{
   const textValue = e.target.value;
   // window.location.replace(`/?text=${textValue}`) ;
   window.history.pushState({}, '', `/?text=${textValue}`);
   getParam()
+  handleTextParameter()
 }
-
 
 const getParam = ()=>{
   const urlParams = new URLSearchParams(window.location.search);
@@ -15,12 +14,28 @@ const getParam = ()=>{
   console.log('textパラメータが変更されました:', textParam);
 }
 
+const handleTextParameter = ()=>{
+  const urlParams = new URLSearchParams(window.location.search);
+  const textParam = urlParams.get('text');
+  const textElement = window.document.getElementById('js-text');
+  if(textElement){
+    textElement.innerText = String(textParam)
+  }
+  
+}
+
+
 function App() {
   return (
     <div className="App">
-      <div className="text">
-        <label className="text__label">テキスト入力</label>
-        <input className="text__input" type={'text'} onChange={(e)=>{changeQueryParam(e)}}/>
+      <div>
+        <div className="text">
+          <label className="text__label">テキスト入力</label>
+          <input className="text__input" type={'text'} onChange={(e)=>{changeQueryParam(e)}}/>
+        </div>
+        <div>
+            <p id="js-text"></p>
+        </div>
       </div>
     </div>
   );
