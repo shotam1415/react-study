9d6abfd245227690d562833c0f2b39ee4b6d4c72
shotam1415 <shotam1415@gmail.com>
2023-07-05 11:20:07 +0900

    tailwindに書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -2,84 +2,85 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [firstNameState, setFirstNameState] = useState("");
-  const [lastNameState, setLastNameState] = useState("");
+    const [firstNameState, setFirstNameState] = useState("");
+    const [lastNameState, setLastNameState] = useState("");
 
-  //リロード時にパラメータをStateに格納する。
-  useEffect(() => {
-    setTextStateAction();
-  }, []);
+    //リロード時にパラメータをStateに格納する。
+    useEffect(() => {
+        setTextStateAction();
+    }, []);
 
-  //入力した値をパラメータに入れる
-  const changeQueryParam = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const targetInputName = e.target.name;
-    const targetInputValue = e.target.value;
-    if (targetInputName === "firstName") {
-      window.history.pushState({}, "", `/?firstName=${targetInputValue}&lastName=${lastNameState}`);
-      setFirstNameState(targetInputValue);
-    }
-    if (targetInputName === "lastName") {
-      window.history.pushState({}, "", `/?firstName=${firstNameState}&lastName=${targetInputValue}`);
-      setLastNameState(targetInputValue);
-    }
-  };
+    //入力した値をパラメータに入れる
+    const changeQueryParam = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const targetInputName = e.target.name;
+        const targetInputValue = e.target.value;
+        if (targetInputName === "firstName") {
+            window.history.pushState({}, "", `/?firstName=${targetInputValue}&lastName=${lastNameState}`);
+            setFirstNameState(targetInputValue);
+        }
+        if (targetInputName === "lastName") {
+            window.history.pushState({}, "", `/?firstName=${firstNameState}&lastName=${targetInputValue}`);
+            setLastNameState(targetInputValue);
+        }
+    };
 
-  const setTextStateAction = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const firstNameParam = urlParams.get("firstName") ? urlParams.get("firstName") : "";
-    const lastNameParam = urlParams.get("lastName") ? urlParams.get("lastName") : "";
-    if (firstNameParam !== null && lastNameParam) {
-      setFirstNameState(firstNameParam);
-      setLastNameState(lastNameParam);
-    }
-  };
+    const setTextStateAction = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const firstNameParam = urlParams.get("firstName") ? urlParams.get("firstName") : "";
+        const lastNameParam = urlParams.get("lastName") ? urlParams.get("lastName") : "";
+        if (firstNameParam !== null && lastNameParam) {
+            setFirstNameState(firstNameParam);
+            setLastNameState(lastNameParam);
+        }
+    };
 
-  const doReload = () => {
-    window.location.reload();
-  };
+    const doReload = () => {
+        window.location.reload();
+    };
 
-  return (
-    <div className="App">
-      <div>
-        <div>
-          <p>一つずつ文字列で呼び出した例</p>
-          <div className="text">
-            <label className="text__label">姓</label>
-            <input
-              className="text__input"
-              name="firstName"
-              type={"text"}
-              value={firstNameState}
-              onChange={(e) => {
-                changeQueryParam(e);
-              }}
-            />
-          </div>
-          <div className="text">
-            <label className="text__label">名</label>
-            <input
-              className="text__input"
-              name="lastName"
-              type={"text"}
-              value={lastNameState}
-              onChange={(e) => {
-                changeQueryParam(e);
-              }}
-            />
-          </div>
-          <div>
-            <p>
-              私の名前は{firstNameState}
-              {lastNameState}です
-            </p>
-          </div>
-          <button type="button" onClick={() => doReload()}>
-            更新する
-          </button>
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div>
+                <div className="border p-10">
+                    <div className="mb-4">
+                        <div className="mb-4">
+                            <label className="block">姓</label>
+                            <input
+                                className="border"
+                                name="firstName"
+                                type={"text"}
+                                value={firstNameState}
+                                onChange={(e) => {
+                                    changeQueryParam(e);
+                                }}
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block">名</label>
+                            <input
+                                className="border"
+                                name="lastName"
+                                type={"text"}
+                                value={lastNameState}
+                                onChange={(e) => {
+                                    changeQueryParam(e);
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <p>
+                            私の名前は{firstNameState}
+                            {lastNameState}です
+                        </p>
+                    </div>
+                    <button type="button" className="border p-2" onClick={() => doReload()}>
+                        更新する
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
