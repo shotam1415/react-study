1af95816d31ab410925ad63e585dcd191e820d52
shotam1415 <shotam1415@gmail.com>
2023-07-05 11:06:01 +0900

    入力欄を2個にした

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,34 @@ import "./App.css";
 import { useState,useEffect } from "react";
 
 function App() {
-  const [textState,setTextState] = useState('')
+  const [firstNameState,setFirstNameState] = useState('')
+  const [lastNameState,setLastNameState] = useState('')
 
   //リロード時にパラメータをStateに格納する。
   useEffect(()=>{
-    setIsText()
+    setTextStateAction()
   },[])
   
   //入力した値をパラメータに入れる
   const changeQueryParam = (e:React.ChangeEvent<HTMLInputElement>)=>{
-    const textValue = e.target.value;
-    window.history.pushState({}, '', `/?text=${textValue}`);
-    setIsText()
+    const targetInputName = e.target.name;
+    const targetInputValue = e.target.value;
+    if(targetInputName === 'firstName'){
+      window.history.pushState({}, '', `/?firstName=${targetInputValue}&lastName=${lastNameState}`);
+      setFirstNameState(targetInputValue)
+    }if(targetInputName === 'lastName'){
+      window.history.pushState({}, '', `/?firstName=${firstNameState}&lastName=${targetInputValue}`);
+      setLastNameState(targetInputValue)
+    }
   }
 
-  const setIsText = ()=>{
+  const setTextStateAction = ()=>{
       const urlParams = new URLSearchParams(window.location.search);
-      const textParam = urlParams.get('text') ? urlParams.get('text'):"";
-      if(textParam !== null){
-        setTextState(textParam)
+      const firstNameParam = urlParams.get('firstName') ? urlParams.get('firstName'):"";
+      const lastNameParam = urlParams.get('lastName') ? urlParams.get('lastName'):"";
+      if(firstNameParam !== null && lastNameParam){
+        setFirstNameState(firstNameParam)
+        setLastNameState(lastNameParam)
       }
   }
 
@@ -32,12 +41,15 @@ function App() {
     <div className="App">
       <div>
         <div className="text">
-          <label className="text__label">テキスト入力</label>
-          <input className="text__input" type={'text'} value={textState} onChange={(e)=>{changeQueryParam(e)}}/>
+          <label className="text__label">姓</label>
+          <input className="text__input" name="firstName" type={'text'} value={firstNameState} onChange={(e)=>{changeQueryParam(e)}}/>
+        </div>
+        <div className="text">
+          <label className="text__label">名</label>
+          <input className="text__input" name="lastName" type={'text'} value={lastNameState} onChange={(e)=>{changeQueryParam(e)}}/>
         </div>
         <div>
-            <p>パラメータの内容が表示されます。</p>
-            <p>{textState}</p>
+            <p>私の名前は{firstNameState}{lastNameState}です</p>
         </div>
         <button type="button" onClick={()=>doReload()}>更新する</button>
       </div>
