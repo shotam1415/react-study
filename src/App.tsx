12b5978c0a12dfd2e3f4cc9cd38a764eacf1f6d1
shotam1415 <shotam1415@gmail.com>
2023-07-06 11:24:38 +0900

    プレビュー画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -60,13 +60,15 @@ function App() {
 
     return (
         <div className="flex items-center justify-center h-screen">
-            <div>
-                <div className="border p-10 mb-10">
+            <div className="flex w-full justify-between max-w-4xl">
+                <div className="w-2/6">
+                <h2 className="text-center mb-4 font-bold text-xl">入力画面</h2>
+                <div className="border p-10">
                     <div className="mb-4">
                         <div className="mb-4">
                             <label className="block">姓</label>
                             <input
-                                className="border"
+                                className="border block w-full"
                                 name="firstName"
                                 type={"text"}
                                 value={NameState.firstName}
@@ -78,7 +80,7 @@ function App() {
                         <div className="">
                             <label className="block">名</label>
                             <input
-                                className="border"
+                                className="border block w-full"
                                 name="lastName"
                                 type={"text"}
                                 value={NameState.lastName}
@@ -88,10 +90,25 @@ function App() {
                             />
                         </div>
                     </div>
-                    {!areAllPropertiesEmpty(NameState)&&
-                        <div className="mb-4">
-                            <p>私の名前は{NameState.firstName}{NameState.lastName}です</p>
-                        </div>
+                </div>
+                </div>
+                <div className="w-3/6">
+                    <h2 className="text-center mb-4 font-bold text-xl">プレビュー画面</h2>
+                    {!areAllPropertiesEmpty(NameState) && 
+                    <div className="border p-10">
+                        {NameState.firstName &&
+                            <dd className="flex items-start gap-2 mb-4 last:mb-0">
+                                <dt>姓：</dt>
+                                <dl>{NameState.firstName}</dl>
+                            </dd>
+                        }
+                        {NameState.lastName &&
+                            <dd className="flex items-start gap-2 mb-4 last:mb-0">
+                                <dt>名：</dt>
+                                <dl>{NameState.lastName}</dl>
+                            </dd>
+                        }
+                    </div>
                     }
                 </div>
             </div>
